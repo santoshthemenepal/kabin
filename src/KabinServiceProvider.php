d84b4ffbaf723402715cbd9277a48896946f5057
Santosh Dheke <santoshdheke46@gmail.com>
2022-06-29 11:33:32 +0545

    asdf

---
+++ src/KabinServiceProvider.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Module\Admin;
+
+use App\View\Components\Header;
+use Illuminate\Support\Facades\Blade;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\ServiceProvider;
+use Module\Admin\Middleware\AdminMiddleware;
+use Module\Admin\Middleware\AuthMiddleware;
+use Module\Admin\Middleware\GueshAuthMiddleware;
+use Module\Admin\Middleware\SuperAdminMiddleware;
+use Module\Admin\Observers\ChatObaserver;
+use Module\Admin\View\Components\DashboardDiv;
+use Module\Common\Models\Chat;
+
+class KabinServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->loadRoutesFrom(__DIR__.'/route.php');
+    }
+}
---
+++ src/route.php
@@ -0,0 +1,5 @@
+<?php
+
+\Illuminate\Support\Facades\Route::get('kabin',function(){
+    dd('this is kabin');
+});
